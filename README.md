29903428b6d9d5072645f1298beac4f661519461
Gusachenko Valentin <vgusachenko@gmail.com>
2021-06-10 00:58:38 +0300

    commit

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ drivers from :
 https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso
 
 
-#       "shutdown_command": "%WINDIR%\\System32\\Sysprep\\sysprep.exe /generalize /oobe /shutdown /unattend:A:\\unattend.xml",
+packer build -only="qemu" windows2019.json
--- powershell/download_puppet.ps1
+++ powershell/download_puppet.ps1
@@ -1,5 +1,4 @@
 $puppetagentx64_download_path = "https://downloads.puppetlabs.com/windows/puppet6/puppet-agent-6.22.1-x64.msi"
 
-Write-Host "downloading"
 $WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("$puppetagentx64_download_path","C:\install\puppetagent-x64.msi")
+$WebClient.DownloadFile("$puppetagentx64_download_path","C:\puppet-agent_x64.msi")
