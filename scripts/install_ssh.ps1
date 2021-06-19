d15bf5fd8fdfa87f5e3a5ebd778265cdd1f9ca0c
Gusachenko Valentin <vgusachenko@gmail.com>
2021-06-19 13:52:44 +0300

    Start-Service sshd

--- scripts/install_ssh.ps1
+++ scripts/install_ssh.ps1
@@ -5,8 +5,8 @@ Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 
 Set-Service sshd -StartupType Automatic
 Set-Service ssh-agent -StartupType Automatic
-Stop-Service sshd
-Stop-Service ssh-agent
+Start-Service sshd
+Start-Service ssh-agent
 
 New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
 
